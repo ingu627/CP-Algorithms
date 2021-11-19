cb24d317c18fa9ca115fdd56f45fdeb653a6603f
ingu627 <ingu627@gmail.com>
2021-11-19 21:39:45 +0900

    7단원 중간까지

---
+++ Baekjoon/steps_7/1157.py
@@ -0,0 +1,24 @@
+# 문제
+# 알파벳 대소문자로 된 단어가 주어지면, 이 단어에서 가장 많이 사용된 알파벳이 무엇인지 알아내는 프로그램을 작성하시오. 단, 대문자와 소문자를 구분하지 않는다.
+
+# 입력
+# 첫째 줄에 알파벳 대소문자로 이루어진 단어가 주어진다. 주어지는 단어의 길이는 1,000,000을 넘지 않는다.
+
+# 출력
+# 첫째 줄에 이 단어에서 가장 많이 사용된 알파벳을 대문자로 출력한다. 단, 가장 많이 사용된 알파벳이 여러 개 존재하는 경우에는 ?를 출력한다.
+
+words = input().upper()
+uniq_words=list(set(words))
+
+cnt_list = []
+for x in uniq_words:
+    cnt = words.count(x)
+    cnt_list.append(cnt)
+
+if cnt_list.count(max(cnt_list)) > 1:
+    print('?')
+else:
+    max_index = cnt_list.index(max(cnt_list))
+    print(uniq_words[max_index])
+
+
